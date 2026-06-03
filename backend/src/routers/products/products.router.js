6f1f1f0db3df40d85989f19f8e6a96ebd404488a
innocent <innocentkamesa05@gmail.com>
2026-06-03 09:54:35 +0200

    setting up routers structure

---
+++ backend/src/routers/products/products.router.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const productsRouter = express.Router();
+
+//productsRouter.get('/', getAllProducts);
+//productsRouter.get('/:id', getProductById);
+//productsRouter.post('/', createProduct);
+//productsRouter.put('/:id', updateProduct);
+//productsRouter.delete('/:id', deleteProduct);
+
+export default productsRouter;
